96b74e9f72719152f744cd477b536a7ca13ab623
PavanKarthik <karthikpavan370@gmail.com>
2023-12-12 14:55:24 +0530

    files and background added

---
+++ CVMS/client/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <img src='http://thewowstyle.com/wp-content/uploads/2015/01/free-beautiful-place-wallpaper-hd-173.jpg'></img>
+  );
+}
+
+export default App;
